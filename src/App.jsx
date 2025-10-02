622ae11ce926d6fd12daa98781ec6f0196f10cf0
quangvu0407 <trantuyenquang0407@gmail.com>
2025-10-02 11:59:11 +0700

    theme

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import { pink } from '@mui/material/colors'
 import HomeIcon from '@mui/icons-material/Home'
+import Typography from '@mui/material/Typography'
 
 function App() {
 
   return (
     <>
       <div>Quang Quang</div>
+
+      <Typography variant='body2' color="text.secondary">Test Typo</Typography>
+
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
